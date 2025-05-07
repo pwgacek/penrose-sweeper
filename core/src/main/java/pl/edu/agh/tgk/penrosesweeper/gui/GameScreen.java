48f4f15f8297b81344def2f101b7e605017df473
Paweł Gacek <pawelgacek.pl@gmail.com>
2025-05-07 20:51:06 +0200

    added simple GameOverDialog

--- core/src/main/java/pl/edu/agh/tgk/penrosesweeper/gui/GameScreen.java
+++ core/src/main/java/pl/edu/agh/tgk/penrosesweeper/gui/GameScreen.java
@@ -11,6 +11,7 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import pl.edu.agh.tgk.penrosesweeper.gui.dialog.GameOverDialog;
 import pl.edu.agh.tgk.penrosesweeper.gui.font.NumbersFont;
 import pl.edu.agh.tgk.penrosesweeper.gui.label.TimerLabel;
 import pl.edu.agh.tgk.penrosesweeper.gui.texture.ExplosionTexture;
@@ -28,8 +29,9 @@ public class GameScreen implements Screen {
     private static final Difficulty DIFFICULTY = Difficulty.EASY;
     private final Board board;
 
-    private Stage stage;
-    private Stage timerStage;
+    private Stage mainStage;
+    private Stage boardStage;
+    private GameOverDialog gameOverDialog;
     private ShapeRenderer shapeRenderer;
     private SpriteBatch spriteBatch;
     private NumbersFont font;
@@ -49,15 +51,19 @@ public class GameScreen implements Screen {
     @Override
     public void show() {
         camera = new OrthographicCamera();
-        stage = new Stage(new FitViewport(SCREEN_SIZE, SCREEN_SIZE, camera));
-        timerStage = new Stage(new ScreenViewport());
+        boardStage = new Stage(new FitViewport(SCREEN_SIZE, SCREEN_SIZE, camera));
+        mainStage = new Stage(new ScreenViewport());
+
+        Gdx.input.setInputProcessor(mainStage);
+
         shapeRenderer = new ShapeRenderer();
         spriteBatch = new SpriteBatch();
 
         font = new NumbersFont(spriteBatch, BOARD_SIZE);
         explosionTexture = new ExplosionTexture(spriteBatch, BOARD_SIZE);
         flagTexture = new FlagTexture(spriteBatch, BOARD_SIZE);
-        timerLabel = new TimerLabel(timerStage);
+        timerLabel = new TimerLabel(mainStage);
+        gameOverDialog = new GameOverDialog();
     }
 
 
@@ -67,11 +73,9 @@ public class GameScreen implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 
-        timerStage.act(delta);
-        timerStage.getViewport().apply();
-        timerStage.draw();
+        boardStage.act(delta);
+        boardStage.getViewport().apply();
 
-        stage.getViewport().apply();
 
         shapeRenderer.setProjectionMatrix(camera.combined);
         spriteBatch.setProjectionMatrix(camera.combined);
@@ -101,6 +105,7 @@ public class GameScreen implements Screen {
                     if (tile.uncover()) {
                         if (tile.isMine()) {
                             isGameOver = true;
+                            gameOverDialog.showWithDelay(mainStage, 0.5f);
                         }
                     }
                 });
@@ -121,6 +126,8 @@ public class GameScreen implements Screen {
             }
         }
 
+        mainStage.getViewport().apply();
+        mainStage.draw();
     }
 
     private void updateTimer() {
@@ -130,15 +137,16 @@ public class GameScreen implements Screen {
 
     private Vector2 getMouseCoordinates() {
         Vector2 screenCords = new Vector2(Gdx.input.getX(), Gdx.input.getY());
-        return stage.getViewport().unproject(screenCords);
+        return boardStage.getViewport().unproject(screenCords);
     }
 
     @Override
     public void resize(int width, int height) {
-        stage.getViewport().update(width, height, true);
-        timerStage.getViewport().update(width, height, true);
-        timerStage.getViewport().setScreenPosition(0, 0);
-        timerLabel.setPosition(0, timerStage.getViewport().getWorldHeight() - timerLabel.getHeight());
+        boardStage.getViewport().update(width, height, true);
+        mainStage.getViewport().update(width, height, true);
+        mainStage.getViewport().setScreenPosition(0, 0);
+        gameOverDialog.setPosition((width - gameOverDialog.getWidth())/ 2f  , (height - gameOverDialog.getHeight()) / 2f);
+        timerLabel.setPosition(0, mainStage.getViewport().getWorldHeight() - timerLabel.getHeight());
 
         camera.position.set(0, 0, 0);
         camera.update();
@@ -161,8 +169,8 @@ public class GameScreen implements Screen {
 
     @Override
     public void dispose() {
-        stage.dispose();
-        timerStage.dispose();
+        boardStage.dispose();
+        mainStage.dispose();
         font.dispose();
         spriteBatch.dispose();
         shapeRenderer.dispose();
---
+++ core/src/main/java/pl/edu/agh/tgk/penrosesweeper/gui/dialog/GameOverDialog.java
@@ -0,0 +1,46 @@
+package pl.edu.agh.tgk.penrosesweeper.gui.dialog;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.Timer;
+import pl.edu.agh.tgk.penrosesweeper.gui.GameScreen;
+import pl.edu.agh.tgk.penrosesweeper.gui.Main;
+
+public class GameOverDialog extends Dialog {
+
+    public GameOverDialog() {
+        super("Game Over", new Skin(Gdx.files.internal("uiskin.json")));
+
+        this.button("Restart", true);
+        this.button("Exit", false);
+        this.text("Game Over! You hit a mine.");
+        this.setMovable(false);
+    }
+
+    @Override
+    protected void result(Object object) {
+        if ((boolean) object) {
+            restartGame();
+        } else {
+            Gdx.app.exit();
+        }
+    }
+
+    public void showWithDelay(Stage stage, float delay) {
+        Timer.schedule(new Timer.Task()
+        {
+            @Override
+            public void run()
+            {
+                show(stage);
+            }
+        }, delay);
+    }
+
+
+    private void restartGame() {
+        ((Main) Gdx.app.getApplicationListener()).setScreen(new GameScreen());
+    }
+}
